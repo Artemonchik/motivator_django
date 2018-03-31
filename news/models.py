5ce64953b98cfb6de93ad5af1211817a454f23d6
Artem <artem_tarasov678@mail.ru>
2018-03-31 14:54:25 +0600

    I done smth

--- news/models.py
+++ news/models.py
@@ -3,7 +3,15 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    pass
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    follow_him = models.ManyToManyField('self', symmetrical=False, related_name='follow_he')
+    stars = models.IntegerField()
+    completed_tasks = models.IntegerField()
+    # Foreing Key from Posts
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
 
 
 class Post(models.Model):
@@ -11,3 +19,6 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
